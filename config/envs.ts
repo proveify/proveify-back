f063185f7b5ab3b9895cbc7fe49ca097bf43b567
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2025-03-08 12:34:21 -0500

    default PORT

--- config/envs.ts
+++ config/envs.ts
@@ -1,4 +1,4 @@
-export const APP_PORT = process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 3000;
+export const APP_PORT = process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 3001;
 
 export const APP_IS_PROD = process.env.APP_ENV === "production";
 export const APP_IS_TESTING = process.env.APP_ENV === "testing";
