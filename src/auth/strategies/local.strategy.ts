aa2c2a5efb60bc6895aa85b3a33e72f33beab94c
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2025-03-24 15:53:13 -0500

    fix LocalStragy

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,19 +2,28 @@ import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "../auth.service";
+import { ContextIdFactory, ModuleRef } from "@nestjs/core";
 
 import type { TokenPayload } from "../interfaces/auth.interface";
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
-    public constructor(private authService: AuthService) {
+export class LocalStrategy extends PassportStrategy(Strategy, "local") {
+    public constructor(private moduleRef: ModuleRef) {
         super({
             usernameField: "email",
+            passReqToCallback: true,
         });
     }
 
-    public async validate(email: string, password: string): Promise<TokenPayload> {
-        const user = await this.authService.validateUser(email, password);
+    public async validate(
+        request: Request,
+        email: string,
+        password: string,
+    ): Promise<TokenPayload> {
+        const contextId = ContextIdFactory.getByRequest(request);
+        const authService = await this.moduleRef.resolve(AuthService, contextId);
+
+        const user = await authService.validateUser(email, password);
 
         if (!user) {
             throw new UnauthorizedException("Invalid credentials, check your email and password");
