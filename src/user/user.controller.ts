069810ae6275ad64d157edae9d056f53fe61e659
HannerB <hannerb48@gmail.com>
2025-04-04 22:16:15 -0500

    implement user profile retrieval and response mapping; add UserNotFoundException and UserResponseInterceptor.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards, UseInterceptors } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { JwtAuthGuard } from "@app/auth/guards/jwt.guard";
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { UserResponse } from "./interfaces/user-response/user-response.interface";
+import { Request } from "express";
+import { TokenPayload } from "@app/auth/interfaces/auth.interface";
+import { UserResponseInterceptor } from "./interceptors/user-response/user-response.interceptor";
 
+@ApiTags("Users")
 @Controller("users")
+@UseInterceptors(UserResponseInterceptor)
 export class UserController {
-    public userService: UserService;
+    public constructor(private readonly userService: UserService) {}
 
-    public constructor(userService: UserService) {
-        this.userService = userService;
+    @Get("profile")
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: "Get logged user information" })
+    @ApiResponse({ status: 200, type: UserResponse, description: "User profile data" })
+    public async getUserProfile(
+        @Req() req: Request & { user: TokenPayload },
+    ): Promise<UserResponse> {
+        return await this.userService.getUserProfile(req.user.id);
     }
 }
