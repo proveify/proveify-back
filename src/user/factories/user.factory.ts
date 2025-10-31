33e102617fba9777b072802e4be064ac8f922ddd
HannerB <hannerb48@gmail.com>
2025-10-31 16:54:14 -0500

    fix: correct casing of 'provider' in UserInput type definition.

--- src/user/factories/user.factory.ts
+++ src/user/factories/user.factory.ts
@@ -3,17 +3,14 @@ import { ProviderFactory } from "@app/provider/factories/provider.factory";
 import { UserEntity } from "@app/user/entities/user.entity";
 import { Prisma, Users as UserModel } from "@prisma/client";
 
-type UserInput = Prisma.UsersGetPayload<{ include: { Provider?: true } }> | UserModel;
+type UserInput = Prisma.UsersGetPayload<{ include: { provider?: true } }> | UserModel;
 
 @Injectable()
 export class UserFactory {
     public constructor(private readonly providerFactory: ProviderFactory) {}
 
     public async create(user: UserInput): Promise<UserEntity> {
-        const providerData =
-            "provider" in user && user.provider
-                ? (user.provider as Prisma.ProvidersGetPayload<object>)
-                : null;
+        const providerData = "provider" in user && user.provider ? user.provider : null;
 
         const data = {
             ...user,
