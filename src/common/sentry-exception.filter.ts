f3803ae9cdc286656ccb17d3928d9400b947bff3
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2026-01-07 22:06:48 -0500

    Simplify `SentryFilter` by capturing all exceptions without status checks

--- src/common/sentry-exception.filter.ts
+++ src/common/sentry-exception.filter.ts
@@ -1,20 +1,11 @@
-import { Catch, ArgumentsHost, HttpStatus, HttpException } from "@nestjs/common";
+import { Catch, ArgumentsHost } from "@nestjs/common";
 import { BaseExceptionFilter } from "@nestjs/core";
 import * as Sentry from "@sentry/node";
 
 @Catch()
 export class SentryFilter extends BaseExceptionFilter {
     public catch(exception: unknown, host: ArgumentsHost): void {
-        let status = HttpStatus.INTERNAL_SERVER_ERROR;
-
-        if (exception instanceof HttpException) {
-            status = exception.getStatus();
-        }
-
-        if (status >= HttpStatus.INTERNAL_SERVER_ERROR) {
-            Sentry.captureException(exception);
-        }
-
+        Sentry.captureException(exception);
         super.catch(exception, host);
     }
 }
